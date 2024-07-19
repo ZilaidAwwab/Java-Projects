45f8d7ac9cb8561c4869204c54c265360627d135
ZilaidAwwab <zilaidawwab786@gmail.com>
2024-07-20 00:02:12 +0500

    Teacher Class Implemented

--- SchoolManagementSystem/src/Teacher.java
+++ SchoolManagementSystem/src/Teacher.java
@@ -1,2 +1,40 @@
 public class Teacher {
+    private int id;
+    private String name;
+    private int salary;
+    private int salaryEarned;
+
+    public Teacher(int id, String name, int salary) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.salaryEarned = 0;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public int getSalary() {
+        return this.salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void receiveSalary(int salary) {
+        salaryEarned += salary;
+        School.updateTotalMoneySpent(salary);
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher's name: " + name + "\n"
+                + "Total salary earned so far: $" + salaryEarned;
+    }
 }
